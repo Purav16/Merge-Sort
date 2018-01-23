60b7fc6c3ca387b64345687239a8df7cddea4774
Purav16 <puravdesai57@gmail.com>
2018-01-23 00:37:49 -0800

    Quick Sort

---
+++ Quick Sort.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Qs  {
+    public static void main(String[] args) {
+        int SIZE = 10;
+        int MAX = 20;
+        int[] x;
+        x = new int[SIZE];
+        Random generator = new Random();
+        for (int i = 0; i < x.length; i++) {
+            x[i] = generator.nextInt(MAX);
+        }
+
+
+
+
+        System.out.println(Arrays.toString(x));
+
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
+        System.out.println(Arrays.toString(x));
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (arr == null || arr.length == 0)
+            return;
+
+        if (low >= high)
+            return;
+
+        // pick the pivot
+        int middle = low + (high - low) / 2;
+        int pivot = arr[middle];
+
+        // make left < pivot and right > pivot
+        int i = low, j = high;
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // recursively sort two sub parts
+        if (low < j)
+            quickSort(arr, low, j);
+
+        if (high > i)
+            quickSort(arr, i, high);
+    }
+}
